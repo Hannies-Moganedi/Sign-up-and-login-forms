1416e676e00ef11b29496e4a14935fe747034506
Hannies-Moganedi <164213753+Hannies-Moganedi@users.noreply.github.com>
2024-06-16 20:55:38 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,13 @@
+<?php
 
+$servername = "Your server name";
+$dBUsername = "database username";
+$dBPassword = "";
+$dBName = "database name";
+
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+if (!$conn) {
+    die("Connection failed: ".mysqli_connect_error());
+}
+?>
