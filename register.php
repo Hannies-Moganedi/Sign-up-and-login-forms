eeaf749220eed4ae9b15be56552d4cba6454552f
Hannies-Moganedi <164213753+Hannies-Moganedi@users.noreply.github.com>
2024-06-16 21:17:24 +0200

    Update register.php

--- register.php
+++ register.php
@@ -6,7 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Registration</title>
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
 
   <!-- Link to custom stylesheet and Font Awesome icons -->
   <link rel="stylesheet" href="styles/register.css" />
